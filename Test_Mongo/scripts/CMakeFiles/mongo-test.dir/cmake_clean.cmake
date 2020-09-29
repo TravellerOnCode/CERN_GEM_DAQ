92a70358495c27333cc604a986cbb3b0b8fa6666
TravellerOnCode <avijitchakraborty1998@gmail.com>
2020-09-29 19:40:34 +0530

    Cmake additions

---
+++ Test_Mongo/scripts/CMakeFiles/mongo-test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mongo-test.dir/write.cpp.o"
+  "mongo-test.pdb"
+  "mongo-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mongo-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
